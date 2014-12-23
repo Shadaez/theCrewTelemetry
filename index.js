31846163e8590f0c32f50289c823e4b06dbf0733
Justin Wheeler <justintymewheeler@gmail.com>
2014-12-23 00:25:19 -0800

    Added new 'message' event which returns raw data so that it can be parsed manually with the parse method. Also now only parses data if it's being used in a callback, rather than at every message received.

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 module.exports = function(port){
+  port = port || 1337;
   var dgram = require('dgram'),
-  server = dgram.createSocket('udp4'),
-  data = {
-    time : 0,
-    angularVelocity: [0,0,0],
-    orientation: [0,0,0],
-    acceleration: [0,0,0],
-    velocity: [0,0,0],
-    position: [0,0,0],
-    gameid: 0
-  },
-  callbacks = {error: [],
-    data: []};
+  callbacks = {error: [], data: [], message: []},
+  ports = Array.isArray(port) ? port : [port],
+  returner;
 
+  ports.forEach(function(port){
+    server = dgram.createSocket('udp4')
     server.on('error', function (err) {
       callback('error', err);
     });
 
-    server.on('message', function (msg) {
-      data = {
-        time: msg.readUInt32LE(0),
-        angularVelocity: [msg.readFloatLE(4), msg.readFloatLE(8), msg.readFloatLE(12)],
-        orientation: [msg.readFloatLE(16), msg.readFloatLE(20), msg.readFloatLE(24)],
-        acceleration: [msg.readFloatLE(28), msg.readFloatLE(32), msg.readFloatLE(36)],
-        velocity: [msg.readFloatLE(40), msg.readFloatLE(44), msg.readFloatLE(48)],
-        position: [msg.readUInt32LE(52), msg.readUInt32LE(56), msg.readUInt32LE(60)],
-        gameID: msg.readUInt32LE(64)
-      };
-      callback('data', data);
+    server.on('message', function (msg, c) {
+      callback('data', msg, c, port);
     });
 
-    server.bind(port || 1337);
+    server.bind(port);
+  });
 
-    function callback(e, data){
-      var arr = callbacks[e] || [];
-      arr.forEach(function(callback){
-        callback(data);
-      });
-    }
-
-    return {
-      on: function(e, callback){
-        callbacks[e] = callbacks[e] || [];
-        callbacks[e].push(callback);
-      },
-      data: function(){
-        return data;
-      },
-      server: function(){
-        return server;
+  function callback(e, msg, c, port){
+    try{
+      if(e == 'data'){
+        callbacks['message'].forEach(function(callback){
+          callback(msg, c, port);
+        });
+        if (callbacks.data.length >= 1) {
+          msg = parseData(msg, c, port);
+          callbacks['data'].forEach(function(callback){
+            callback(data);
+          });
+        }
       }
-    };
+    } catch(err) {
+      callback('error', err);
+    }
+  }
+
+  function parseData(msg, c, port){
+    return {
+      time: msg.readUInt32LE(0),
+      angularVelocity: [msg.readFloatLE(4), msg.readFloatLE(8), msg.readFloatLE(12)],
+      orientation: [msg.readFloatLE(16), msg.readFloatLE(20), msg.readFloatLE(24)],
+      acceleration: [msg.readFloatLE(28), msg.readFloatLE(32), msg.readFloatLE(36)],
+      velocity: [msg.readFloatLE(40), msg.readFloatLE(44), msg.readFloatLE(48)],
+      position: [msg.readUInt32LE(52), msg.readUInt32LE(56), msg.readUInt32LE(60)],
+      gameID: msg.readUInt32LE(64),
+      client: {ip: c.address, port: port}
+    }
+  }
+
+  returner = {
+    on: function(e, callback){
+      callbacks[e] = callbacks[e] || [];
+      callbacks[e].push(callback);
+    },
+    server: function(){
+      return server;
+    },
+    parse: parseData
   };
+
+  return returner;
+};
